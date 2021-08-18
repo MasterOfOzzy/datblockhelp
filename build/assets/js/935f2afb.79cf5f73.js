52878aef0b19cd97f7cd4f99bd99648d1a48d65d
MasterOfOzzy <63637308+MasterOfOzzy@users.noreply.github.com>
2021-08-18 22:03:58 +0100

    fixes1

--- build/assets/js/935f2afb.79cf5f73.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdatblockhelp=self.webpackChunkdatblockhelp||[]).push([[53],{1109:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","banner":"none","isLast":true,"docsSidebars":{"tutorialSidebar":[{"type":"link","label":"Tutorial Intro","href":"/datblockhelp/docs/intro"}]}}')}}]);
